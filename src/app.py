cd555e4d1c832b58d474a24a750305afc8108dc3
hafid4827 <luceres12joshua@gmail.com>
2021-02-21 00:25:29 -0500

    add command line

--- src/app.py
+++ src/app.py
@@ -1,63 +1,71 @@
 import hashlib as hs
-import os
 from time import time
-import sys, getopt
-# pip install -U memory_profiler
-#Con el decorador @profile podemos ver la memoria que está usando, para ello necesitamos tener memory_profiler instalado, podemos hacerlo con:
-
-
-
-def lineParam():
-    pass
+from os import path
+from argparse import ArgumentParser
 
 def funcDicstCall(paramSha, callDictId):
     dictShaFunc = {
-        '--md5': hs.md5(paramSha),
-        '--sha1': hs.sha1(paramSha),
-        '--sha224': hs.sha224(paramSha),
-        '--sha256': hs.sha256(paramSha),
-        '--sha384': hs.sha384(paramSha),
-        '--sha512': hs.sha512(paramSha),
-        '--blake2b': hs.blake2b(paramSha),
-        '--blake2s': hs.blake2s(paramSha),
-        '--sha3_224': hs.sha3_224(paramSha),
-        '--sha3_256': hs.sha3_256(paramSha),
-        '--sha3_384': hs.sha3_384(paramSha),
-        '--sha3_512': hs.sha3_512(paramSha),
-        #'--shake_128': hs.shake_128(x.encode('utf-8')).hexdigest(),
-        #'--shake_256': hs.shake_256(x.encode('utf-8')).hexdigest(),
+        'md5': hs.md5(paramSha),
+        'sha1': hs.sha1(paramSha),
+        'sha224': hs.sha224(paramSha),
+        'sha256': hs.sha256(paramSha),
+        'sha384': hs.sha384(paramSha),
+        'sha512': hs.sha512(paramSha),
+        'blake2b': hs.blake2b(paramSha),
+        'blake2s': hs.blake2s(paramSha),
+        'sha3_224': hs.sha3_224(paramSha),
+        'sha3_256': hs.sha3_256(paramSha),
+        'sha3_384': hs.sha3_384(paramSha),
+        'sha3_512': hs.sha3_512(paramSha),
+        #'shake_128': hs.shake_128(x.encode('utf-8')).hexdigest(),
+        #'shake_256': hs.shake_256(x.encode('utf-8')).hexdigest(),
     }
     return dictShaFunc[callDictId].hexdigest()
 
-def _CRI(_Encripting, HashSelect, rutDict):
-    openDict = open(os.path.abspath(rutDict),'r').readlines()
+def _CRIV1(_Encripting, HashSelect, rutDict):
+    openDict = open(path.abspath(rutDict),'r').readlines()
     return openDict[list(map(lambda iterDict : funcDicstCall(iterDict.replace('\n', '').encode('utf-8'),HashSelect),openDict)).index(_Encripting)].replace('\n', '')
 
-def _CRIV2(rutDict, na):
-    openDict = open(os.path.abspath(rutDict),'r').readlines()
+def _CRIV2(_Encripting, rutDict, HashSelect):
+    openDict = open(path.abspath(rutDict),'r').readlines()
     for i in openDict:
-        if na == funcDicstCall(i.replace('\n', '').encode('utf-8'), '--sha512'):
+        if _Encripting == funcDicstCall(i.replace('\n', '').encode('utf-8'), HashSelect):
             return i
         
-def _CRIV3(rutDict, na):
-    return [i.replace('\n', '') for i in open(os.path.abspath(rutDict),'r').readlines() if na == funcDicstCall(i.replace('\n', '').encode('utf-8'), '--sha512')]
+def _CRIV3(_Encripting, rutDict, HashSelect):
+    return [i.replace('\n', '') for i in open(path.abspath(rutDict),'r').readlines() if _Encripting == funcDicstCall(i.replace('\n', '').encode('utf-8'), HashSelect)]
+
+def pruebados(Def, Sha, Option, Redirect):
+        if Def == 'CRIV1':
+            return _CRIV1(
+                _Encripting=Sha,
+                HashSelect=Option, 
+                rutDict=Redirect,)
+        elif Def == 'CRIV2':
+            return _CRIV2(
+                _Encripting=Sha,
+                HashSelect=Option, 
+                rutDict=Redirect)
+        elif Def == 'CRIV3':
+            return _CRIV3(
+                _Encripting=Sha,
+                HashSelect=Option, 
+                rutDict=Redirect)
 
 
 if __name__ == '__main__':
-    print('''''')
+    parser = ArgumentParser(description = ' -D CRYV1 -S dc517d7ed2c7e9ec2870a... -O sha512 -R ../dict/dictPassword.txt')
+    parser.add_argument('-D', '--DEF',) # opcion de cualquier funcion que exite 
+    parser.add_argument('-S', '--SHA',)# criptografia encriptada
+    parser.add_argument('-O', '--OPTION',) # funcion seletivo hash
+    parser.add_argument('-R', '--REDIRECT',) # direccion opcional del diccionario
+    args = parser.parse_args()
     #OP_Encripting = input('Introduce tu hash: ')
     #OP_HashSelect = input('introduce tipo de criptografia: ')
-    start_time = time()
-    print(_CRI(_Encripting='dc517d7ed2c7e9ec2870aabefbd9985f395ebbca1a0a100807cbdd489796ce91dce58500c79e20c18ffe322b339465999c5cf37e5937db6b42a08ac48b9a74e6',
-               HashSelect='--sha512', 
-               rutDict='../dict/dictPassword.txt'
-               ))
-    print(time() - start_time)
-    start_time = time()
-    print(*_CRIV3('../dict/dictPassword.txt',
-        'dc517d7ed2c7e9ec2870aabefbd9985f395ebbca1a0a100807cbdd489796ce91dce58500c79e20c18ffe322b339465999c5cf37e5937db6b42a08ac48b9a74e6')
-        )
-    print(time() - start_time)
-    print(sys.argv)
-
-
+    initTime = time()
+    print(*pruebados(
+        Def = args.DEF,
+        Sha= args.SHA,
+        Option= args.OPTION,
+        Redirect= args.REDIRECT,))
+    print(time() - initTime)

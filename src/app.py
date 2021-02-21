f152f5eceb81a75c2a7c1f4ac7c5afaf3cc4c2ad
hafid4827 <luceres12joshua@gmail.com>
2021-02-21 14:36:55 -0500

    helpme for user

--- src/app.py
+++ src/app.py
@@ -55,35 +55,32 @@ def argumentLinePy(Def, Sha, Option, Redirect):
                 HashSelect=Option, 
                 rutDict=Redirect)
 
-
-# 7ca973a353c97334ee3f51c3cd7d116ac2dfe7579d6a2a62c6d3cb2aabd8042c61f0aa4177cde0c94a3102d27033f5f7156169f97422adc31c9cb3160da7a823
-# dc517d7ed2c7e9ec2870aabefbd9985f395ebbca1a0a100807cbdd489796ce91dce58500c79e20c18ffe322b339465999c5cf37e5937db6b42a08ac48b9a74e6
-# ce7aaac2b65b110c2c52d2636441037f64468ebec5f741df29e7a7876fd3c2e2389a7fdfd9df866e9ce697718e4bea33d10c5f22d02134e9a2c62628c949088f
-
 if __name__ == '__main__':
-    parser = ArgumentParser(description = ' -D CRYV1 -S dc517d7ed2c7e9ec2870a... -O sha512 -R ../dict/dictPassword.txt')
-    parser.add_argument('-D', '--DEF',)
-    parser.add_argument('-S', '--SHA',)
-    parser.add_argument('-L', '--LEN',)
-    parser.add_argument('-T', '--TEXT',)
-    parser.add_argument('-O', '--OPTION',)
-    parser.add_argument('-R', '--REDIRECT',)
+
+    orsep = '\n\t'+'-'*30
+
+    parser = ArgumentParser(description='decripcion degeneral')
+    parser.add_argument('-D', '--DEF', help='#')
+    parser.add_argument('-S', '--SHA', help='#')
+    parser.add_argument('-L', '--LEN', help='#')
+    parser.add_argument('-T', '--TEXT', help='#')
+    parser.add_argument('-O', '--OPTION', help='#')
+    parser.add_argument('-R', '--REDIRECT', help='#')
     args = parser.parse_args()
-    print('\n\t'+'-'*30)
+
+    print(orsep)
+
     initTime = time()
-    print(f'\tContraseña decifrada:', argumentLinePy(
+
+    print('\tContraseña decifrada:', argumentLinePy(
         Def = args.DEF,
         Sha= args.SHA,
         Option= args.OPTION,
         Redirect= args.REDIRECT,
         ))
-    print(f'\tTiempo de ejecucion: {time() - initTime}')
-    print('\t'+'-'*30)
-# PENDIENTES
-# ayuda para usuarios 
-# ejemplos para usuarios 
-# cantidad de caracteres para intentar identificar el tipo de criptografia yu no recorrerlos todo de un solo tiro
-# convertir de caracteres a criptografia
 
+    if args.TEXT != None:
+        print(funcDicstCall(args.TEXT.encode('utf-8'), args.OPTION))
 
-
+    print('\tTiempo: ',time() - initTime )
+    print(orsep)
